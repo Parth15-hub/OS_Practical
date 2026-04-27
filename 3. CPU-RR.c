d30a7ca5f3be4e677be93c87bda698b337c436d4
Parth15-hub <parthkjadhav15@gmail.com>
2026-04-27 19:48:06 +0530

    Update and rename CPU-RR.c to 3. CPU-RR.c

--- 3. CPU-RR.c
+++ 3. CPU-RR.c
@@ -2,17 +2,18 @@
 
 int main() {
     int n, i, time = 0, remain, tq;
-    int bt[20], rt[20], ct[20], tat[20], wt[20];
+    int at[20], bt[20], rt[20], ct[20], tat[20], wt[20];
+    int done[20] = {0};
     float total_wt = 0, total_tat = 0;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    // Input Burst Time
+    // Input AT & BT
     for(i = 0; i < n; i++) {
-        printf("Enter BT for P%d: ", i+1);
-        scanf("%d", &bt[i]);
-        rt[i] = bt[i];   // remaining time
+        printf("Enter AT and BT for P%d: ", i+1);
+        scanf("%d %d", &at[i], &bt[i]);
+        rt[i] = bt[i];
     }
 
     printf("Enter Time Quantum: ");
@@ -20,10 +21,15 @@ int main() {
 
     remain = n;
 
-    // Round Robin Logic
+    // Round Robin with AT
     while(remain > 0) {
+        int executed = 0;
+
         for(i = 0; i < n; i++) {
-            if(rt[i] > 0) {
+            if(at[i] <= time && rt[i] > 0) {
+
+                executed = 1;
+
                 if(rt[i] <= tq) {
                     time += rt[i];
                     ct[i] = time;
@@ -35,11 +41,15 @@ int main() {
                 }
             }
         }
+
+        // CPU Idle
+        if(!executed)
+            time++;
     }
 
-    // TAT & WT
+    // Calculate TAT & WT
     for(i = 0; i < n; i++) {
-        tat[i] = ct[i];        // AT = 0
+        tat[i] = ct[i] - at[i];
         wt[i] = tat[i] - bt[i];
 
         total_tat += tat[i];
@@ -47,13 +57,13 @@ int main() {
     }
 
     // Output
-    printf("\nP\tBT\tCT\tTAT\tWT\n");
+    printf("\nP\tAT\tBT\tCT\tTAT\tWT\n");
     for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t%d\t%d\n", i+1, bt[i], ct[i], tat[i], wt[i]);
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
 
-    printf("\nAverage TAT = %.2f", total_tat/(float)n);
-    printf("\nAverage WT = %.2f", total_wt/(float)n);
+    printf("\nAverage TAT = %.2f", total_tat/n);
+    printf("\nAverage WT = %.2f", total_wt/n);
 
     return 0;
 }
